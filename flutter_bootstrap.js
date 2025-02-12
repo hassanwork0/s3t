6bad5d815b7841f99e2f23acdc533cb1084e405e
Normal-user69 <hm3446092@gmail.com>
2025-02-12 18:48:22 +0200

    Update the game to alpha 1.5

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4078266356"
+    serviceWorkerVersion: "1533937759"
   }
 });
